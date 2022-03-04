36c0ed3937127521361858b98a0630778b22143f
J. Kevin Corcoran <whimsicalstarfish@gmail.com>
2022-03-03 17:24:40 -0700

    Initial Commit

---
+++ Shared/ToDoLiciousApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoLiciousApp.swift
+//  Shared
+//
+//  Created by J. Kevin Corcoran on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoLiciousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
